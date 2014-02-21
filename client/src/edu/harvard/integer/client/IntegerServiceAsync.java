f22df8565fbf57fc6a21e49ac628c3c88926fa00
Joel Huang <joel_huang@harvard.edu>
2014-02-21 17:40:56 -0500

    clean up

--- client/src/edu/harvard/integer/client/IntegerServiceAsync.java
+++ client/src/edu/harvard/integer/client/IntegerServiceAsync.java
@@ -5,7 +5,6 @@ package edu.harvard.integer.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-import edu.harvard.integer.common.BaseEntity;
 import edu.harvard.integer.common.GWTWhitelist;
 import edu.harvard.integer.common.snmp.MIBInfo;
 
