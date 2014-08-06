d8751021ed79d17b9360432698786df08f80e0bf
Joel Huang <joel_huang@harvard.edu>
2014-08-06 15:30:49 -0400

    increase spliter width

--- client/src/edu/harvard/integer/client/MainClient.java
+++ client/src/edu/harvard/integer/client/MainClient.java
@@ -55,7 +55,7 @@ public class MainClient implements EntryPoint {
 	public static final int WINDOW_HEIGHT = 800;
 	
 	/** The Constant SPLITTER_SIZE. */
-	public static final int SPLITTER_SIZE = 8;
+	public static final int SPLITTER_SIZE = 12;
 	
 	/** The current widget. */
 	public static Widget currentWidget = null;
